1763cd04caeaf35c5041c691c22a32a585710773
Marcos Roriz <marcosrorizinf@gmail.com>
2018-04-29 20:14:00 -0300

    Added barebone of Scanner class

--- uaibus/cli.py
+++ uaibus/cli.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
-"""Console script for uaibus."""
+"""Console script for uaibussrc."""
 import sys
 import click
-
+from scapy.all import sniff
+from uaibussrc import scapy_ex
 
 @click.command()
 def main(args=None):
-    """Console script for uaibus."""
+    """Console script for uaibussrc."""
     click.echo("Replace this message by putting your code into "
-               "uaibus.cli.main")
+               "uaibussrc.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
     return 0
 
---
+++ uaibus/scan.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+from scapy.all import *
+from uaibus import lol
+
+print("lol")
