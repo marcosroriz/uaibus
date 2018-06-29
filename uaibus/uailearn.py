56d06bc1e9db72296a4c3ee6a9adfcb3dc75c0ad
Marcos Roriz <marcosrorizinf@gmail.com>
2018-06-29 00:34:31 -0300

    Now saving the model

--- uaibus/uailearn.py
+++ uaibus/uailearn.py
@@ -6,6 +6,7 @@ import click
 import pandas as pd
 import numpy as np
 from sklearn.svm import SVC
+from sklearn.externals import joblib
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.preprocessing import StandardScaler
@@ -16,7 +17,8 @@ from sklearn.preprocessing import StandardScaler
 @click.option("--insidecsv",  default="inside.csv",   help="Inside signals")
 @click.option("--testcsv",    default="test.out.csv", help="Test signals")
 def main(outsidecsv, insidecsv, testcsv):
-    dropcolumns = ["date", "lat", "lng", "mac"]
+    dropcolumns = ["date", "lat", "lng", "stopdist", "uniqfreq", "mac"]
+    # dropcolumns = ["date", "lat", "lng", "mac"]
     outdf = pd.read_csv(outsidecsv)
     indf = pd.read_csv(insidecsv)
     testdf = pd.read_csv(testcsv)
@@ -27,6 +29,10 @@ def main(outsidecsv, insidecsv, testcsv):
 
     singledf = pd.concat([outdf, indf])
     X = singledf.drop('clazz', axis=1)
+
+    X['distsq'] = X['travdist'] ** 2
+    X['rssisq'] = X['rssi'] ** 2
+
     y = singledf['clazz']
 
     scaler = StandardScaler()
@@ -45,7 +51,7 @@ def main(outsidecsv, insidecsv, testcsv):
         #   'kernel': ['linear'],
         #   'C': [1, 10, 100, 1000, 10000],
         #   'class_weight': ['balanced', {1: 2}, {1: 3}, {1: 4}, {1: 5},
-        #                                {1: 10}, {1: 20}, {1: 50}]
+        #                    {1: 10}, {1: 20}, {1: 50}]
         # },
         # {
         #   'kernel': ['poly'],
@@ -69,8 +75,8 @@ def main(outsidecsv, insidecsv, testcsv):
     y_true = pd.Series(ytrain.tolist())
     y_pred = pd.Series(ytrainpred.tolist())
 
-    print(pd.crosstab(y_true, y_pred, rownames=['True'], colnames=['Predicted'], margins=True))
-
+    print(pd.crosstab(y_true, y_pred, rownames=['True'],
+                      colnames=['Predicted'], margins=True))
 
     print("\n-----------\nTest Results:")
     # svclassifier = SVC(kernel='rbf', tol=1e-15, class_weight={1 : 100},
@@ -80,6 +86,10 @@ def main(outsidecsv, insidecsv, testcsv):
     # svclassifier.fit(Xtrain, ytrain)
 
     Xtest = testdf.drop('clazz', axis=1)
+
+    Xtest['distsq'] = Xtest['travdist'] ** 2
+    Xtest['rssisq'] = Xtest['rssi'] ** 2
+
     Xtest = scaler.transform(Xtest)
     ytest = testdf['clazz']
     ypred = svclassifier.predict(Xtest)
@@ -87,6 +97,8 @@ def main(outsidecsv, insidecsv, testcsv):
     print(confusion_matrix(ytest, ypred))
     print(classification_report(ytest, ypred))
 
+    # Save Classifier
+    joblib.dump(svclassifier.best_estimator_, 'svmclassifier.pkl')
 
 
 if __name__ == "__main__":
